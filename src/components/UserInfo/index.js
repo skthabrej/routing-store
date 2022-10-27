e36a44663e924b6893992f477be4f1b885fee978
Shaik Thabrej Basha <skthabrej@gmail.com>
2022-10-27 12:33:33 +0530

    fetch-and-route

--- src/components/UserInfo/index.js
+++ src/components/UserInfo/index.js
@@ -2,7 +2,7 @@ import './index.css'
 
 const UserInfo = () => (
   <div className="user-container">
-    <img src="https://i.ibb.co/0fKv90y/people.png" alt="profile" className="profile"/>
+    <img src="https://i.postimg.cc/rw0GfpTP/boy-g5f911686e-1280.png" alt="profile" className="profile"/>
     <h1 className="user-name">Ambrose</h1>
     <p className="user-designation">reactJS developer at US</p>
   </div>
